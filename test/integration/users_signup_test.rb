f35e54c6e4e54d14471f616172797d067d623679
Chaos Predictor <kuzi81@gmail.com>
2016-05-14 16:45:57 +0400

    additional fail tests, pass till 7.4

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,10 +1,34 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-	test "invalid signup information" do
+	test "invalid signup name information" do
 		get signup_path
 		assert_no_difference 'User.count' do
-			post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
+			post users_path, user: { name: "", email: "user@invalid.com", password: "foobar", password_confirmation: "foobar" }
+		end
+		assert_template 'users/new'
+	end
+	
+	test "invalid signup email information" do
+		get signup_path
+		assert_no_difference 'User.count' do
+			post users_path, user: { name: "Dima", email: "user@invalid", password: "foobar", password_confirmation: "foobar" }
+		end
+		assert_template 'users/new'
+	end
+	
+	test "invalid signup password information" do
+		get signup_path
+		assert_no_difference 'User.count' do
+			post users_path, user: { name: "Dima", email: "user@invalid.com", password: "foo", password_confirmation: "foo" }
+		end
+		assert_template 'users/new'
+	end
+	
+	test "invalid signup password confirmation information" do
+		get signup_path
+		assert_no_difference 'User.count' do
+			post users_path, user: { name: "Dima", email: "user@invalid.com", password: "foobar1", password_confirmation: "foobar2" }
 		end
 		assert_template 'users/new'
 	end
