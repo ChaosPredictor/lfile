47edd4047ad680aa114c2942316b6b65c5b8d981
Chaos Predictor <kuzi81@gmail.com>
2016-05-14 15:29:47 +0400

    test fail till 7.4

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,6 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-	
 	def setup
 		@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
 	end
