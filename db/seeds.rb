36bf4dfb09a77eac910849cb3ff5d3598d529c4a
Chaos Predictor <kuzi81@gmail.com>
2016-05-27 15:03:07 +0300

    finish user edit, update, index and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true)
 
-9.times do |n|
+99.times do |n|
 	name = Faker::Name.name
 	email = "example-#{n+1}@railstutorial.org"
 	password = "password"
