83f7215b205e136c6f5fbcbc440fa39e0d06135f
Chaos Predictor <kuzi81@gmail.com>
2016-05-28 16:33:12 +0300

    test pass, till 9.6 exercise 3

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 	def setup
-		@user = users(:michael)
-		@other_user = users(:archer)
+		@user        = 	users(:michael)
+		@other_user  = users(:archer)
+		@other_user2  = users(:lana)		
 	end
 	
 	test "should get new" do
@@ -37,6 +38,15 @@ class UsersControllerTest < ActionController::TestCase
 		patch :update, id: @user, user: { name: @user.name, email: @user.email }
 		assert flash.empty?
 		assert_redirected_to root_url
+	end
+	
+	test "should not allow the admin attribute to be edited via the web" do
+		log_in_as(@other_user)
+		assert_not @other_user.admin?
+		patch :update, id: @other_user, user: { password: "password", 
+																						password_confirmation: "password",
+																						admin: true }
+		assert_not @other_user.admin?
 	end
 	
 	test "should redirect index when not logged in" do
