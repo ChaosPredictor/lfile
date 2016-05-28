e2219f83f570049f1cb372b084881909c32a7698
Chaos Predictor <kuzi81@gmail.com>
2016-05-28 15:50:38 +0300

    test pass till 9.6 exercies 2

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 	
+	def setup
+		@user = users(:michael)
+		@user2 = users(:dmitry)
+	end
+	
 	test "links home page" do
 		get root_path
 		assert_template 'static_pages/home'
@@ -104,6 +109,38 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 		assert_select "ul.nav"
 		assert_select "ul.navbar-nav"
 		assert_select "ul.navbar-right"
+	end
+	
+	test "logged-in user" do
+		log_in_as(@user)
+		assert_not flash.empty?
+		assert_equal flash[:success], "Welcome Home!"
+		assert_redirected_to @user
+		get root_path
+		assert_template 'static_pages/home'
+		assert_select "a[href=?]", root_path, count: 2
+		assert_select "a[href=?]", help_path
+		assert_select "a[href=?]", about_path
+		assert_select "a[href=?]", contact_path
+		assert_select "a[href=?]", signup_path, count: 0
+		assert_select "a[href=?]", login_path, count: 0
+		assert_select "a[href=?]", user_path(@user), count: 1		
+		assert_select "a[href=?]", edit_user_path(@user), count: 1		
+		assert_select "a[href=?]", logout_path, count: 1
+	end
+	
+	test "logged-out user" do
+		get root_path
+		assert_template 'static_pages/home'
+		assert_select "a[href=?]", root_path, count: 2
+		assert_select "a[href=?]", help_path
+		assert_select "a[href=?]", about_path
+		assert_select "a[href=?]", contact_path
+		assert_select "a[href=?]", signup_path, count: 1
+		assert_select "a[href=?]", login_path, count: 1
+		assert_select "a[href=?]", user_path(@user), count: 0		
+		assert_select "a[href=?]", edit_user_path(@user), count: 0		
+		assert_select "a[href=?]", logout_path, count: 0
 	end
 	
 end
