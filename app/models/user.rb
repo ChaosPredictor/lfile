1eaf875df72270b4f808a55f8c41b19c505f108c
Chaos Predictor <kuzi81@gmail.com>
2016-06-03 23:00:24 +0300

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,25 +12,6 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 	
-	# Returns the hash digest of the given string.
-	#def User.digest(string)
-	#	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-	#	BCrypt::Password.create(string, cost: cost)
-	#end
-	#same as
-	#def self.digest(string)
-	#	cost = ActiveModel::SecurePassword.min_cost ?BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-	#	BCrypt::Password.create(string, cost: cost)
-	#end
-	
-	#def User.new_token
-	#		SecureRandom.urlsafe_base64
-	#end
-	#same as
-	#def self.new_token
-	#		SecureRandom.urlsafe_base64
-	#end
-	#amd same as
 	class << self
 		def digest(string)
 			cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -43,25 +24,11 @@ class User < ActiveRecord::Base
 		end
 	end
 	
-	#def User.new_token
-	#		SecureRandom.urlsafe_base64
-	#end
-	#same as
-	#def self.new_token
-	#		SecureRandom.urlsafe_base64
-	#end
-	
 	def remember
 		self.remember_token = User.new_token
 		update_attribute(:remember_digest, User.digest(remember_token))
 	end
 	
-	# Returns true if the given token matches the digest.
-	#def authenticated?(remember_token)
-	#	return false if remember_digest.nil?
-	#	BCrypt::Password.new(remember_digest).is_password?(remember_token)
-	#end
-	# Returns true if the given token matches the digest.
 	def authenticated?(attribute, token)
 		digest = send("#{attribute}_digest")
 		return false if digest.nil?
@@ -75,6 +42,17 @@ class User < ActiveRecord::Base
 	# Forgets a user.
 	def forget
 		update_attribute(:remember_digest, nil)
+	end
+	
+	# Activates an account.
+	def activate
+		update_attribute(:activated, true)
+		update_attribute(:activated_at, Time.zone.now)
+	end
+	
+	# Sends activation email.
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
 	end
 	
 	private
