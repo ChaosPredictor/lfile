baae14d8ac3a432f78accbd3499d33ce2b9965ab
Chaos Predictor <kuzi81@gmail.com>
2016-06-20 00:44:58 +0300

    test pass, till 12.5.2

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -4,6 +4,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
 
 	def setup
 		@user = users(:michael)
+		@other = users(:archer)
 	end
 	
 	test "profile display" do
@@ -15,6 +16,23 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
 		assert_match @user.microposts.count.to_s, response.body
 		assert_select 'div.pagination'
 		@user.microposts.paginate(page: 1).each do |micropost|
+			assert_match micropost.content, response.body
+		end
+	end
+	
+	test "home page display" do
+		log_in_as(@user)
+		get root_path
+		assert_template 'static_pages/home'
+		assert_select 'title', full_title("")
+		assert_select 'h3', text: @user.name
+		assert_select 'a>img.gravatar'
+		id = @user.id
+		following_ids = @user.following_ids
+		allPost = Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+		assert_match allPost.count.to_s, response.body
+		assert_select '.container'
+		allPost.paginate(page: 1).each do |micropost|			
 			assert_match micropost.content, response.body
 		end
 	end
