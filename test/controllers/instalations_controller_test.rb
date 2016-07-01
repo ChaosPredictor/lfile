bd31bf3ea5b17a76004c949c13cf67ae01c5a0c3
Chaos Predictor <kuzi81@gmail.com>
2016-07-02 03:45:53 +0400

    test pass, controller instalation test path after create

--- test/controllers/instalations_controller_test.rb
+++ test/controllers/instalations_controller_test.rb
@@ -24,6 +24,22 @@ class InstalationsControllerTest < ActionController::TestCase
 		assert_redirected_to instalations_path
   end
 	
+	test "should create when admin" do
+		log_in_as(@user_admin)
+		assert_difference 'Instalation.count', 1 do
+			post :create, instalation: { name: "Lorem", version: "1.1", os: "new" }
+		end
+		assert_redirected_to instalations_path
+	end
+	
+	test "should create when logged in" do
+		log_in_as(@user_notadmin)
+		assert_difference 'Instalation.count', 1 do
+			post :create, instalation: { name: "Lorem", version: "1.1", os: "new" }
+		end
+		assert_redirected_to instalations_path
+	end
+	
 	test "should redirect create when not logged in" do
 		assert_no_difference 'Instalation.count' do
 			post :create, instalation: { name: "Lorem", version: "1.1", os: "new" }
