686737be01e59cbcb9f843de5c24a1a5c7a36cd8
Chaos Predictor <kuzi81@gmail.com>
2016-07-17 01:59:00 +0400

    fix of Gemfile and change name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_linux_apps_installer_session'
+Rails.application.config.session_store :cookie_store, key: '_lfile_session'
