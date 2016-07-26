366934e1ded7ad15920e607535c1876dab24852e
Chaos Predictor <kuzi81@gmail.com>
2016-07-26 16:53:24 +0400

    test pass, config.action_mailer to ActionMailer::Base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 #		:enable_starttls_auto => true
 #	}
 
-	config.action_mailer.smtp_settings = { 
+	ActionMailer::Base.smtp_settings = { 
 		:address => 'smtp.yandex.ru',
 		:port => '25',
 		:authentication => :plain,
