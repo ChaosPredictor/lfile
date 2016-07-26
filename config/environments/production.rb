76069b4a7764deea502ecdd61130101ac229f106
Chaos Predictor <kuzi81@gmail.com>
2016-07-26 20:55:47 +0400

    add tls to email production settings production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,8 @@ Rails.application.configure do
 
 	ActionMailer::Base.smtp_settings = { 
 		:address => 'smtp.yandex.ru',
-		:portt => '587',
+		:portt => '587', #465
+		:tls true,
 		:authentication => :plain,
 		:user_name => ENV['email_username'],
 		:password => ENV['email_password'],
