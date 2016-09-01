d571819e2d5f2ad6bd871d224bd4222f955ab2c2
Chaos Predictor <kuzi81@gmail.com>
2016-09-01 16:15:14 +0400

    test pass, add test.run - to run all the test in one commant, small change in download test

--- spec/models/spec.rb
+++ spec/models/spec.rb
@@ -11,10 +11,20 @@
       #expect( DownloadHelpers::download_content ).to include temp.txt
 			#expect(DownloadHelpers::download_content).to have_content('temp.txt')
 			page.response_headers['Content-Type'].should == "text/lfile"
+			page.status_code.should == 200
+			#page.response_headers['Content-Type'].must_equal "text/lfile"
 			#page.response_headers['Content-Disposition'].should == "attachment; filename=\"temp.txt\""
 			header = page.response_headers['Content-Disposition']
 			header.should match /^attachment/
 			header.should match /filename=\"temp.txt\"$/
+			#puts page.html
+			#page.body.has_content? "sudo"
+			#visit a_path
+			#expect(page).to have_text("Login successful.")
+			#page.must_has_content "Schubidu"
+  		#page.must_have_content "U13"
+  		#page.wont_have_content "5000"
+			#page.should match "sudo"
 			expect(page).to have_http_status(:success)
     end
 	end
