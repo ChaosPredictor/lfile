733d1e9077bbafe61fc3de85335f144d5704ee84
Chaos Predictor <kuzi81@gmail.com>
2016-09-07 18:47:10 +0400

    test not pass, working on show installation as logged in not admin not owner, taking a break

--- test/controllers/installations_controller_test.rb
+++ test/controllers/installations_controller_test.rb
@@ -4,10 +4,12 @@ class InstallationsControllerTest < ActionController::TestCase
 	def setup
 		@installation     = installations(:gimp)
 		@user_admin       = users(:michael)
-		@user_notadmin    = users(:archer)		
+		@user_notadmin    = users(:archer)
+		@user             = users(:lana)		
+		@installation.update(name: "GIMP", version: "1.1", os: "linux", source_link: "gimp.com", user_id: @user.id)
 	end
 	
-	#Not Log in user
+	#Not Logged in user
 	#######################################
 	
 	test "not logged in should index" do
@@ -15,15 +17,6 @@ class InstallationsControllerTest < ActionController::TestCase
 		assert_equal 200, response.status
     assert_response :success
 		assert_select 'h1', text: "All Installations", count: 1
-	end
-
-	test "not logged in should not show" do
-    get :show, id: @installation
-		assert_equal 302, response.status
-    assert_response :redirect
-		assert_redirected_to login_path
-		assert_not flash.empty?
-		assert_equal  "Please log in.", flash[:danger]
 	end
 
 	test "not logged in should not new" do
@@ -37,8 +30,17 @@ class InstallationsControllerTest < ActionController::TestCase
 	
 	test "not logged in should not create" do
 		assert_no_difference 'Installation.count' do
-			get :create, id: @installation, installation: {  name: "PMIG", version: "1.1", os: "linux", source_link: "pimg.com", user_id: @user_admin.id}
+			get :create, id: @installation, installation: {name: "PMIG", version: "1.1", os: "linux", source_link: "pimg.com", user_id: @user_admin.id}
 		end
+		assert_equal 302, response.status
+    assert_response :redirect
+		assert_redirected_to login_path
+		assert_not flash.empty?
+		assert_equal  "Please log in.", flash[:danger]
+	end
+	
+	test "not logged in should not show" do
+    get :show, id: @installation
 		assert_equal 302, response.status
     assert_response :redirect
 		assert_redirected_to login_path
@@ -76,10 +78,81 @@ class InstallationsControllerTest < ActionController::TestCase
 	end
 	
 	
-
-
+	#Logged in user, not admin
+	#######################################
 	
+	test "logged in, not admin should index" do
+		log_in_as(@user_notadmin)
+		get :index
+		assert_equal 200, response.status
+    assert_response :success
+		assert_select 'h1', text: "All Installations", count: 1
+	end
+
+	test "logged in, not admin should new" do
+		log_in_as(@user_notadmin)
+		get :new
+		assert_equal 200, response.status
+    assert_response :success
+		assert_select 'h1', text: "New Installation", count: 1
+	end
 	
+	test "logged in, not admin should create" do
+		log_in_as(@user_notadmin)
+		assert_difference 'Installation.count', 1 do
+			get :create, id: @installation, installation: {  name: "PMIG", version: "1.1", os: "linux", source_link: "pimg.com", user_id: @user_admin.id}
+		end
+		assert_equal 302, response.status
+    assert_response :redirect
+		assert_redirected_to installations_path
+		assert_not flash.empty?
+		assert_equal  "New Installation Saved!!!", flash[:success]
+	end
+
+	#Logged in user, not admin, not owner
+	#######################################
+		
+	test "logged in, not admin, not owner should not show" do
+		log_in_as(@user_notadmin)
+		get :show, id: @installation.id
+		assert_equal 302, response.status
+    assert_response :redirect
+		assert_redirected_to root_path
+		#assert_not flash.empty?
+		#assert_equal  "Please log in.", flash[:danger]
+	end
+	
+	test "logged in, not admin, not owner should not edit" do
+		log_in_as(@user_notadmin)
+    get :edit, id: @installation
+		assert_equal 302, response.status
+    assert_response :redirect
+		assert_redirected_to root_path
+		#assert_not flash.empty?
+		#assert_equal  "Please log in.", flash[:danger]
+	end
+	
+	test "logged in, not admin, not owner should not update" do
+		log_in_as(@user_notadmin)
+    patch :update, id: @installation, installation: {  name: "PMIG", version: "1.1", os: "linux", source_link: "pimg.com", user_id: @user_admin.id}
+		assert_equal 302, response.status
+    assert_response :redirect
+		assert_redirected_to root_path
+		#assert_not flash.empty?
+		#assert_equal  "Please log in.", flash[:danger]
+	end
+	
+	test "logged in, not admin, not owner should not destroy" do
+		log_in_as(@user_notadmin)	
+		assert_no_difference 'Installation.count' do
+			delete :destroy, id: @installation
+		end
+		assert_equal 302, response.status
+    assert_response :redirect
+		assert_redirected_to root_path
+		#assert_not flash.empty?
+		#assert_equal  "Please log in.", flash[:danger]
+	end
 	
 	
 	
@@ -190,7 +263,7 @@ class InstallationsControllerTest < ActionController::TestCase
 		assert_match @installation.version, "1.1"
 		assert_match @installation.os, "linux"
 		assert_match @installation.source_link, "gimp.com"
-		assert_equal @installation.user_id, 1		
+		assert_equal @installation.user_id, @user.id
 		assert_no_difference 'Installation.count' do
 			patch :update, id: @installation, installation: {name: "PIMG",
 																											version: "1.2",
@@ -204,7 +277,7 @@ class InstallationsControllerTest < ActionController::TestCase
 		assert_match @installation2.version, "1.2"
 		assert_match @installation2.os, "xos"
 		assert_match @installation2.source_link, "pmig.com"
-		assert_equal @installation.user_id, 1
+		assert_equal @installation.user_id, @user.id
 		assert_not_equal @installation.user_id, @user_admin.id
 	end
 	
@@ -266,13 +339,13 @@ class InstallationsControllerTest < ActionController::TestCase
 	test "should edit for admin user without user change" do		
 		log_in_as(@user_admin)
 		@installation = Installation.first
-		assert_equal @installation.user_id, 1
+		assert_equal @installation.user_id, @user.id
 		assert_no_difference 'Installation.count' do
 			patch :update, id: @installation, installation: {user_id: @user_admin.id}
 		end
 		assert_redirected_to installations_path
 		@installation2 = Installation.first
-		assert_equal @installation.user_id, 1
+		assert_equal @installation.user_id, @user.id
 		assert_not_equal @installation.user_id, @user_admin.id		
 	end
 	
@@ -302,24 +375,24 @@ class InstallationsControllerTest < ActionController::TestCase
 	test "should not edit added_by for not admin user" do		
 		log_in_as(@user_notadmin)
 		@installation = Installation.first
-		assert_equal @installation.user_id, 1
+		assert_equal @installation.user_id, @user.id
 		assert_no_difference 'Installation.count' do
 			patch :update, id: @installation, installation: {user_id: @user_admin.id}
 		end
 		assert_redirected_to root_path
 		@installation2 = Installation.first
-		assert_equal @installation2.user_id, 1
+		assert_equal @installation2.user_id, @user.id
 	end
 	
 	test "should not edit added_by for not logedin" do		
 		@installation = Installation.first
-		assert_equal @installation.user_id, 1
+		assert_equal @installation.user_id, @user.id
 		assert_no_difference 'Installation.count' do
 			patch :update, id: @installation, installation: {user_id: @user_admin.id}
 		end
 		assert_redirected_to login_path
 		@installation2 = Installation.first
-		assert_equal @installation2.user_id, 1
+		assert_equal @installation2.user_id, @user.id
 	end
 	
 end
