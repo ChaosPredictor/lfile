a3bc01e9df96bbb86bcd21b88719073f5cf22d73
Chaos Predictor <kuzi81@gmail.com>
2016-09-12 18:16:03 +0400

    test pass, new test for view session link

--- test/views/sessions_link_test.rb
+++ test/views/sessions_link_test.rb
@@ -4,8 +4,8 @@ class SessionsInterfaceTest < ActionDispatch::IntegrationTest
 	#assert_match "#{Line.count} lines", response.body
 	def setup
 		@user_active      = users(:michael)
-		@user_notadmin    = users(:archer)
-		@user             = users(:lana)		
+		@user_unactive    = users(:lana)		
+		@user             = users(:archer)
 	end
 	
 	# Log-in
@@ -30,9 +30,35 @@ class SessionsInterfaceTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", createfile_path, count: 1
 		assert_select "a[href=?]", password_resets_new_path, count: 1
 		assert_select "input[id=session_remember_me]", count: 1
+		assert_select "a[href=?]", "/resend_activation/#{@user_unactive.email}", count: 0
 	end
 	
-	test "log-in page, after" do
+	test "log-in page right email/password unactivated user, after" do
+		log_in_as(@user_unactive)
+		#assert_template 'sessions/new'		
+		assert_redirected_to root_path
+		follow_redirect!	
+		#assert_match "dsfgd", response.body
+		assert_select "a[href=?]", root_path, count: 1
+		assert_select "a[href=?]", help_path, count: 1
+		assert_select "a[href=?]", about_path, count: 1
+		assert_select "a[href=?]", contact_path, count: 1
+		assert_select "a[href=?]", signup_path, count: 1
+		assert_select "a[href=?]", login_path, count: 1
+		assert_select "a[href=?]", users_path, count: 0		
+		assert_select "a[href=?]", edit_user_path(@user_active), count: 0
+		assert_select "a[href=?]", logout_path, count: 0		
+		assert_select "a[href=?]", lines_path, count: 0
+		assert_select "a[href=?]", installations_path, count: 0		
+		assert_select "a[href=?]", operating_systems_path, count: 0		
+		assert_select "a[href=?]", createfile_path, count: 1
+		assert_select "a[href=?]", password_resets_new_path, count: 0
+		assert_select "input[id=session_remember_me]", count: 0
+		assert_select "a[href=?]", "/resend_activation/#{@user_unactive.email}", count: 1
+
+	end
+	
+	test "log-in page right email/password activated user, after" do
 		log_in_as(@user_active)
 		assert_redirected_to root_path
 		follow_redirect!
@@ -51,6 +77,7 @@ class SessionsInterfaceTest < ActionDispatch::IntegrationTest
 		assert_select "a[href=?]", createfile_path, count: 1
 		assert_select "a[href=?]", password_resets_new_path, count: 0
 		assert_select "input[id=session_remember_me]", count: 0
+		assert_select "a[href=?]", "/resend_activation/#{@user_unactive.email}", count: 0
 	end
 	
 end
