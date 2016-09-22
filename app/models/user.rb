7bd0c24fedc36be1339eb072f903b937df9462f6
Chaos Predictor <kuzi81@gmail.com>
2016-09-22 14:10:30 +0400

    test pass, commit before upgradeto rails 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@ class User < ActiveRecord::Base
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 	
 	class << self
+		# Returns the hash digest of the given string.
 		def digest(string)
 			cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 			BCrypt::Password.create(string, cost: cost)
