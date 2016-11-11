e9a62a39f2e86068ca1f1fe6594c1b916f30a49d
Chaos Predictor <kuzi81@gmail.com>
2016-11-11 22:45:22 +0200

    test pass, first commit after long time

--- test/controllers/steps_controller_test.rb
+++ test/controllers/steps_controller_test.rb
@@ -4,4 +4,26 @@ class StepsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+	#def setup
+	#	@step = Step.new(installation_id: 1, line_id: 1, order: 1)
+	#end
+
+	#test "should be valid" do
+	#	assert @step.valid?
+	#end
+
+	#test "should require a installation_id" do
+	#	@step.installation_id = nil
+	#	assert_not @step.valid?
+	#end
+
+	#test "should require a line_id" do
+	#	@step.line_id = nil
+	#	assert_not @step.valid?
+	#end
+
+	#test "should require a order" do
+	#	@step.order = nil
+	#	assert_not @step.valid?
+	#end
 end
